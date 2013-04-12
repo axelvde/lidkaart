d7794e9aeab9f03c91713570ada44dfadc3c4bc0
Tim Daminet <tim.daminet@gmail.com>
2013-04-12 10:57:14 +0200

    update php file with new html/css

--- index.php
+++ index.php
@@ -4,96 +4,109 @@
 		<title>Nieuw Lid</title>
 		<!-- Bootstrap -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+  <link href="css/style.css" rel="stylesheet" media="screen">
+  <script src="http://code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</head>
 	<body>
+ 	<!-- start rij 1 -->
  	<div class="row-fluid">
-  	<div class="span12">
-   	<img src="img/antransp.png" width= '25%' height='25%' />
+  	<div class="span12 header">
+   	<img src="img/antransp.png" width="25%" height="25%" />
    </div>
   </div>
-
-		<img src="img/antransp.png" width= '25%' height='25%' />
-		<script src="http://code.jquery.com/jquery.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-
-		<div class="container-fluid">
-			<div class="row-fluid">
-				<h3>Lidkaart</h3>
-				<img src="animoLogo.jpg" />
-			</div>
-			
-			<form action="lijst.php" method="post">
-
-				<div class="row-fluid">
-					<div class="span2">
-						<label>Voornaam: </label>
-						<input class="input-medium" type="text" name="vNaam" />
-					</div>
-					<div class="span2">
-						<label>Achternaam: </label>
-						<input class="input-medium" type="text" name="aNaam" />
-					</div>
-				</div>
-
-				<div class="row-fluid">
-					<div class="span2">
-						<label>Straat: </label>
-						<input class="input-medium" type="text" name="straat" />
-					</div>
-					<div class="span1">
-						<label>Nummer: </label>
-						<input class="input-mini" type="text"  name="huisnummer" />
-					</div>
-					<div class="span1">
-						<label>Bus: </label>
-						<input class="input-mini" type="text"  name="bus" />
-					</div>
-				</div>
-
-				<div class="row-fluid">
-					<div class="span2">
-						<label>Postcode: </label>
-						<input class="input-mini" type="text"   maxlength="4" name="postcode" />
-					</div>
-
-				</div>
-				<div class="row-fluid">
-					<div class="span2">
-						<label>Email: </label>
-						<input class="input-medium" type="text" name="Email" />
-					</div>
-					<div class="span2">
-						<label>Ik wil medewerker worden:</label></td><td>
-						<input type="checkbox" value="1" name="medewerker" />
-					</div>
-				</div>
-
-				<div class="row-fluid">
-					<div class="span2">
-						<label>Geboortejaar: </label></td><td>
-						<select class="input-small" name="geboorteJaar">
-							<?php
+  
+  <!-- start rij 2 -->
+  <div class="row-fluid content">
+  	<div class="span12">
+   		<h1>Lidkaart</h1>
+   </div>
+  </div>
+  
+  <form class="form-horizontal" action="lijst.php" method="post">
+  <!-- start rij 3 -->
+  <div class="row-fluid content">
+   
+   <!-- start kol 3.1 -->
+   <div class="span6">
+    <div class="control-group">
+     <label class="control-label" for="vNaam">Voornaam</label>
+     <div class="controls">
+     	<input type="text" id="vNaam" placeholder="Voornaam" name="vNaam">
+     </div>
+    </div>
+    <div class="control-group">
+     <label class="control-label" for="aNaam">Achternaam</label>
+     <div class="controls">
+     	<input type="text" id="aNaam" placeholder="Achternaam" name="aNaam">
+     </div>
+    </div>
+    <div class="control-group">
+     <label class="control-label" for="Email">E-mailadres</label>
+     <div class="controls">
+     	<input type="text" id="Email" placeholder="E-mailadres" name="Email">
+     </div>
+    </div>
+    <div class="control-group">
+    <div class="controls">
+      <label class="checkbox">Ik wil medewerker worden
+        <input type="checkbox" value="1" name="medewerker">
+      </label>
+    </div>
+  	</div>
+   <div class="control-group">
+    <div class="controls">
+      <label>Geboortejaar: 
+						<select name="geboorteJaar" class="input-small">
+						<?php
 							for ($i = 1960; $i <= date('Y'); $i++) {
 								echo '<option value="' . $i . '">' . $i . '</option>';
 							}
 							?>
 						</select>
-					</div>
-				</div>
-
-				<div class="row-fluid">
-					<div class="span2">
-						<input type="reset" value="Reset" name="reset" />
-					</div>
-					<div class="span2">
-						<input class="btn btn-primary" type="submit" value="VERZEND" name="submit"/>
-					</div>
-				</div>
-				
-		
-		</form>
-		</div>
-
+      </label>
+    </div>
+  	</div>
+   </div>
+   
+   <!-- start kol 3.2 -->
+   <div class="span6">
+   	<div class="control-group">
+     <label class="control-label" for="straat">Straat</label>
+     <div class="controls">
+     	<input type="text" id="straat" placeholder="Straat" name="straat">
+     </div>
+    </div>
+    <div class="control-group">
+     <label class="control-label" for="huisnummer">Huisnummer</label>
+     <div class="controls">
+     	<input type="text" id="huisnummer" placeholder="Huisnummer" name="huisnummer" class="input-small" >
+     </div>
+    </div>
+    <div class="control-group">
+     <label class="control-label" for="bus">Bus</label>
+     <div class="controls">
+     	<input type="text" id="bus" placeholder="Bus" name="bus" class="input-small" >
+     </div>
+    </div>
+    <div class="control-group">
+     <label class="control-label" for="postcode">Postcode</label>
+     <div class="controls">
+     	<input type="text" id="postcode" placeholder="Postcode" name="postcode" class="input-small" >
+     </div>
+    </div>
+   </div>
+ </div>
+ 
+ <!-- start rij 4 -->
+ <div class="row-fluid content">
+ 	<div class="span12">
+   <input type="reset" value="Reset" name="reset" class="btn btn-large" />
+   <button type="submit" class="btn btn-large btn-primary">Verzenden</button>
+  </div>
+ </div>
+	</form>
 	</body>
 </html>
